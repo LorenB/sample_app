a144bb3a887a8a313ee74e09516374545efe393b
Loren Barcus <loren.barcus@gmail.com>
2011-11-06 12:55:23 -0500

    Added a tile helper and stylesheets

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,9 @@ require 'spec_helper'
 describe PagesController do
   render_views
   
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
@@ -10,8 +13,8 @@ describe PagesController do
     end
     it "should have the right title" do
           get 'home'
-          response.should have_selector("title",
-                            :content => "Ruby on Rails Tutorial Sample App | Home")
+          response.should have_selector("title", 
+                                        :content => "#{@base_title} | Home")
     end
     it "should have a non-blank body" do
       get 'home'
@@ -28,8 +31,7 @@ describe PagesController do
     it "should have the right title" do
           get 'contact'
           response.should have_selector("title",
-                            :content =>
-                              "Ruby on Rails Tutorial Sample App | Contact")
+                            :content => "#{@base_title} | Contact")
     end
   end
   describe "GET 'about'" do
@@ -41,8 +43,7 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_selector("title",
-                        :content =>
-                          "Ruby on Rails Tutorial Sample App | About")
+                        :content => "#{@base_title} | About")
     end
   end
 end
